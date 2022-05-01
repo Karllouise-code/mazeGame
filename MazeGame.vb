ff276fbefb922f70acccd4ab7dfa613bf329ca40
Karl Louise Rito <karl.rito@switchconnect.com.au>
2022-05-01 15:26:59 +0800

    Audio Loop and Stop Audio Function

--- MazeGame.vb
+++ MazeGame.vb
@@ -13,16 +13,17 @@
 
     Private Sub Wall_MouseEnter(sender As Object, e As EventArgs) Handles Label53.MouseEnter, Label51.MouseEnter, Label50.MouseEnter, Label58.MouseEnter, PictureBox9.MouseEnter, PictureBox8.MouseEnter, PictureBox7.MouseEnter, PictureBox6.MouseEnter, PictureBox5.MouseEnter, PictureBox47.MouseEnter, PictureBox46.MouseEnter, PictureBox45.MouseEnter, PictureBox44.MouseEnter, PictureBox43.MouseEnter, PictureBox42.MouseEnter, PictureBox41.MouseEnter, PictureBox40.MouseEnter, PictureBox4.MouseEnter, PictureBox39.MouseEnter, PictureBox38.MouseEnter, PictureBox37.MouseEnter, PictureBox36.MouseEnter, PictureBox35.MouseEnter, PictureBox34.MouseEnter, PictureBox33.MouseEnter, PictureBox32.MouseEnter, PictureBox31.MouseEnter, PictureBox30.MouseEnter, PictureBox3.MouseEnter, PictureBox29.MouseEnter, PictureBox28.MouseEnter, PictureBox27.MouseEnter, PictureBox26.MouseEnter, PictureBox25.MouseEnter, PictureBox24.MouseEnter, PictureBox23.MouseEnter, PictureBox22.MouseEnter, PictureBox21.MouseEnter, PictureBox20.MouseEnter, PictureBox2.MouseEnter, PictureBox19.MouseEnter, PictureBox18.MouseEnter, PictureBox17.MouseEnter, PictureBox16.MouseEnter, PictureBox15.MouseEnter, PictureBox14.MouseEnter, PictureBox13.MouseEnter, PictureBox12.MouseEnter, PictureBox11.MouseEnter, PictureBox10.MouseEnter, PictureBox1.MouseEnter, Label4.MouseEnter, Label3.MouseEnter, Label2.MouseEnter, Label1.MouseEnter, Label6.MouseEnter, Label5.MouseEnter
         Life -= 1
-        MessageBox.Show("Try Again :) " & Environment.NewLine & "Remaining life: " + Life.ToString)
         If Life <> 0 Then
+            MessageBox.Show("Try Again :) " & Environment.NewLine & "Remaining life: " + Life.ToString)
             MoveToStart()
         Else
             Me.Hide()
-            My.Computer.Audio.Play(My.Resources.monsterAudio, AudioPlayMode.Background)
+            My.Computer.Audio.Play(My.Resources.monsterAudio, AudioPlayMode.BackgroundLoop)
             Surprise.Show()
             BeLazy()
             Dim ans = MessageBox.Show("You are out of lives" & Environment.NewLine & "Do you want to try again?", "Information", MessageBoxButtons.YesNo, MessageBoxIcon.Information)
             If ans = DialogResult.Yes Then
+                StopBackgroundSound()
                 Life = 5
                 MoveToStart()
                 Surprise.Hide()
@@ -43,6 +44,10 @@
             Threading.Thread.Sleep(100) ' 3 SECONDS DELAY
             Application.DoEvents()
         Next
+    End Sub
+
+    Sub StopBackgroundSound()
+        My.Computer.Audio.Stop() 'STOP BACKGROUND AUDIO
     End Sub
 
 End Class
